accc7ccbb0d86a39138193cb82af0470059d4402
renbeini <renbeini19911111@yahoo.co.jp>
2016-08-22 15:56:57 +0900

    driver passenger

---
+++ Unblocking/app/src/main/java/com/example/rbnwxj/unblocking/PassengerBrowse.java
@@ -0,0 +1,13 @@
+package com.example.rbnwxj.unblocking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PassengerBrowse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passenger_browse);
+    }
+}
---
+++ Unblocking/app/src/main/java/com/example/rbnwxj/unblocking/driver_browse.java
@@ -0,0 +1,13 @@
+package com.example.rbnwxj.unblocking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class driver_browse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_browse);
+    }
+}
