f7a139f1b2e29fa161e56c9b1c53415f1f29b81a
we444 <wantone333@gmail.com>
2016-08-22 16:34:43 +0900

    Android_client & Linux_server

---
+++ Linux_Server/server.cpp
@@ -0,0 +1,226 @@
+/*
+	1.注册SIGNAL用于子进程回收
+	2.绑定ip和port,获得listener
+	3.epoll-->添加监听listener
+	4.while(1)
+		等待epoll返回监听事件数量(非阻塞),然后轮询处理各个fd
+		|
+		|--服务器自身fd-->处理新连接的客户(添加eproll_event,添加客户数组)
+		|
+		|--客户fd-->recv
+					|
+					|--> =0,客户断开连接-->关闭fd,客户数组剔除,epoll_event剔除
+					|
+					|--> >0,接收客户的消息并处理(fork)
+*/
+
+/*
+完成功能:
+	1.login
+	2.chat|name|msg 聊天指定发送对象	(用户登录时,fd写入mysql)
+	3.chat|all|msg  聊天内容发往所有在线用户
+	4.用户退出时,fd清空为-1
+
+待导入功能:
+	1.登录后,数据库更新登录状态
+	2.客户通过命令获取当前在线用户名单
+	3.是否使用多线程
+	4.用于聊天的fd保存在内存中,而不是每次chat都去读数据库
+
+Bug:
+	register的id自增问题
+*/
+
+#include "utility.h"
+#include "sqllib.h"
+
+void client_connect(int sockfd);
+void client_message(int sockfd);
+void message_route(int sockfd, vector<string> vs);
+	void Login(int sockfd, vector<string> vs);
+	void Register(int sockfd, vector<string> vs);
+	void Chat(int sockfd, vector<string> vs);
+	void Search(int sockfd, vector<string> vs);
+	void Upload(int sockfd, vector<string> vs);
+
+typedef struct
+{
+	string cmd;
+	void (*func)(int, vector<string>);
+}Service;
+
+Service service[] = 
+{
+	{"login", 		Login},
+	{"register", 	Register},
+	{"chat",		Chat},
+	{"search",		Search},
+	{"upload", 		Upload},
+};
+
+int main(int ac, char *av[])
+{
+	signal(SIGCHLD, child_waiter);
+	cs.clear();		// 清理客户信息
+
+	int listener = make_server_socket(IP, PORT);
+	int event_cnt;
+
+	Try(epfd = epoll_create(EPOLL_SIZE))
+
+	epoll_event events[EPOLL_SIZE];
+	epfd_add(epfd, listener, true);		// epoll中注册服务器fd
+
+	while(1)
+	{
+		// -1: epoll_wait will return when events occured
+		// 0: epoll_wait will return immediately, deprecated
+		Try((event_cnt = epoll_wait(epfd, events, EPOLL_SIZE, -1)))
+
+		for(int i = 0; i < event_cnt; i++)
+		{
+			int sockfd = events[i].data.fd;
+
+			if(sockfd == listener)
+				client_connect(sockfd);
+			else
+				client_message(sockfd);	
+		}
+	}
+
+	close(listener);
+	close(epfd);
+	return 0;
+}
+
+void client_connect(int sockfd)
+{
+	int clientfd;
+	sockaddr_in client_address;
+	socklen_t addrlen = sizeof(sockaddr_in);
+
+	Try((clientfd = accept(sockfd, (sockaddr *)&client_address, &addrlen)))
+	cs.push_back(clientfd);
+	epfd_add(epfd, clientfd, true);
+
+	printf("client connection from: %s : % d(IP : port), clientfd = %d \n",
+    		inet_ntoa(client_address.sin_addr),
+    		ntohs(client_address.sin_port),
+    		clientfd);
+}
+
+void client_message(int sockfd)
+{
+	char msg[BUFSIZ], buf[BUFSIZ];
+	bzero(msg, BUFSIZ); bzero(buf, BUFSIZ);
+	int len = recv(sockfd, buf, BUFSIZ, 0);
+	if(0 > len)
+	{
+		myErr;
+	}
+	else if(0 == len)
+	{
+		cout<<"******client exit******"<<endl;
+		cs.remove(sockfd);
+		close(sockfd);
+		epfd_del(epfd, sockfd);		// 不再监视离开的用户的fd
+		// 一旦下线,用于聊天的addr也清空为-1
+		wesql.ClearAddr(sockfd);
+	}
+	else
+	{
+		cout<<buf<<endl;
+		message_route(sockfd, split(buf));
+	}
+}
+
+void message_route(int sockfd, vector<string> vs)
+{
+	if(0 == vs.size())
+		myErr;
+
+	for(int i = 0; i < sizeof(service)/sizeof(service[0]); i++)
+		if(service[i].cmd == vs[0])
+			{service[i].func(sockfd, vs); return;}	// command route
+	
+	Try(send(sockfd, "undefined cmd\r\n", BUFSIZ, 0))
+}
+
+void Login(int sockfd, vector<string> vs)
+{
+	login_info usr;
+	usr.name = vs[1];
+	usr.pwd = vs[2];
+	if(wesql.Login(usr, sockfd))		// 传入clientfd更新数据库信息,用于聊天
+		Try(send(sockfd, "login success\n\n", BUFSIZ, 0))
+	else
+		Try(send(sockfd, "login fail\n", BUFSIZ, 0))
+}
+
+void Register(int sockfd, vector<string> vs)
+{
+	register_info usr;
+	usr.name = vs[1];
+	usr.pwd = vs[2];
+	usr.email = vs[3];
+	if(wesql.Register(usr))
+		Try(send(sockfd, "register success\n", BUFSIZ, 0))
+	else
+		Try(send(sockfd, "register fail\n", BUFSIZ, 0))
+}
+
+void Chat(int sockfd, vector<string> vs)
+{
+	// 为了能发消息给vs[1],读取name=vs[1]的对象此刻数据库中存放的fd
+	string from = wesql.FindNameFromAddr(sockfd);
+	string msg = "<" + from + ">:" + vs[2];
+	if("all" == vs[1])
+	{
+		list<int>::iterator it;
+    	for(it = cs.begin(); it != cs.end(); it++)
+			if(sockfd != *it)
+				Try(send(*it, msg.c_str(), BUFSIZ, 0))// 广播不要发给自己
+	}
+	else
+	{
+		int to = atoi(wesql.FindAddrFromName(vs[1]).c_str());
+		Try(send(to, msg.c_str(), BUFSIZ, 0))
+	}
+}
+
+void Search(int sockfd, vector<string> vs)
+{
+	vector<string> db_res;
+	search_info info;
+	info.date = vs[1];
+	info.start = vs[2];
+	info.end = vs[3];
+
+	db_res = wesql.Search(info);
+
+	if(0 == db_res.size())
+		Try(send(sockfd, "no search results\n", BUFSIZ, 0));
+
+	string msg;
+	vector<string>::iterator it;
+	for(it = db_res.begin(); it != db_res.end(); it++)	// bug 由于client的epoll监听是同一事件,连续send两次消息,client也只能处理一次消息
+		msg += *it + '|';
+	msg += '\n';
+	Try(send(sockfd, msg.c_str(), BUFSIZ, 0));
+}
+
+void Upload(int sockfd, vector<string> vs)
+{
+	carpool_info info;
+	info.name = vs[1];
+	info.date = vs[2];
+	info.start = vs[3];
+	info.end = vs[4];
+	info.price = vs[5];
+	info.seat = vs[6];
+	info.comment = vs[7];
+	if(wesql.Upload(info))
+		Try(send(sockfd, "upload success\n", BUFSIZ, 0))
+	else
+		Try(send(sockfd, "upload fail\n", BUFSIZ, 0))
+}
---
+++ Linux_Server/sqllib.h
@@ -0,0 +1,209 @@
+/*
+	MySQL cmd
+	CREATE TABLE table_name (column_name column_type);
+	新增一列: alter table userinfo add column addr varchar(20) not null;
+	更新数据: udate userinfo set login="NO" where name='we';
+
+*/
+
+#ifndef MYSQL_H_INCLUDED
+#define MYSQL_H_INCLUDED
+
+#include <iostream>
+#include <sstream>
+#include <mysql/mysql.h>
+#include <mysql_connection.h>  
+#include <mysql_driver.h>  
+#include <cppconn/driver.h>  
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+#include <cppconn/prepared_statement.h>
+
+using namespace std;
+using namespace sql;
+
+#define SQL_ADDRESS			("tcp://localhost:3306")
+#define SQL_USER			("root")
+#define SQL_PASSWORD		("333333")
+#define SQL_INIT_ADDR		("-1")
+
+typedef struct
+{
+	string name;
+	string pwd;
+}login_info;
+
+typedef struct
+{
+	string name;
+	string pwd;
+	string email;
+	string sex;
+	string age;
+}register_info;
+
+typedef struct
+{
+	string date;
+	string start;
+	string end;
+}search_info;
+
+typedef struct
+{
+	string name;
+	string date;
+	string start;
+	string end;
+	string price;
+	string seat;
+	string comment;
+}carpool_info;
+
+class WeSQL
+{
+private:
+	Driver *driver;
+	Connection *conn;
+	Statement *stmt;
+	ResultSet *res;
+	PreparedStatement *pstmt;
+public:	
+	WeSQL()
+	{
+		driver = get_driver_instance();
+		conn = driver->connect(SQL_ADDRESS, SQL_USER, SQL_PASSWORD); 
+		conn->setSchema("carpool");
+		stmt = conn->createStatement();
+	}
+	~WeSQL(){	cout<<"析构函数执行完毕"<<endl;};
+/*	bool Create_table()
+	{
+		stmt = conn->createStatement();
+		stmt->execute("DROP TABLE IF EXISTS userinfo");
+		stmt->execute("CREATE TABLE userinfo(id int primary key, name varchar(20), pwd varchar(20), email varchar(50), sex varchar(2), age varchar(2))");
+		stmt->execute("alter table userinfo change id id int auto_increment;");
+		return true;
+	}	*/
+	bool ClearAddr(int addr)	// 用户退出时,必须要清理用户本次登录时记录到数据库的addr为-1	
+	{
+		pstmt = conn->prepareStatement("UPDATE userinfo set addr=(?) where addr=(?)");
+		char c_addr[10];
+		snprintf(c_addr, 10, "%d", addr);		// int的fd转换为char,保存到数据库
+		pstmt->setString(1, SQL_INIT_ADDR);
+		pstmt->setString(2, c_addr);
+		res = pstmt->executeQuery();
+		return true;
+	}
+	bool UpdateAddr(string name, int addr)	// 更新用户本次登录时打开的fd,便于之后聊天
+	{
+		pstmt = conn->prepareStatement("UPDATE userinfo set addr=(?) where name=(?)");
+		char c_addr[10];
+		snprintf(c_addr, 10, "%d", addr);	// int的fd转换为char,保存到数据库
+		pstmt->setString(1, c_addr);
+		pstmt->setString(2, name);
+		res = pstmt->executeQuery();
+		return true;
+	}
+	bool Login(login_info& usr, int addr)	// addr是client此次登录时server打开的fd
+	{
+		pstmt = conn->prepareStatement("SELECT name, pwd FROM userinfo where name=(?) and pwd=(?)");
+		pstmt->setString(1, usr.name);
+		pstmt->setString(2, usr.pwd);
+		res = pstmt->executeQuery();
+
+		if(!res->next())	// user is not exist
+			return false;
+
+		UpdateAddr(usr.name, addr);
+		return true;
+	}
+	bool Register(register_info& usr)		// 确认此用户名或邮箱是否已经注册过,然后添加到数据库
+	{	
+		pstmt = conn->prepareStatement(("SELECT name, email FROM userinfo where name=(?) or email=(?)"));
+		pstmt->setString(1, usr.name);
+		pstmt->setString(2, usr.email);
+		res = pstmt->executeQuery();
+		
+		if(res->next())	
+			return false;	// 如果存在此name | email的用户,则返回false
+
+		pstmt = conn->prepareStatement("INSERT INTO userinfo(name, pwd, email, addr) VALUES (?,?,?,?)");
+	//    pstmt->setInt(1,16);		// BUG: 不录入id会报错,还需要录入id时自动获取自增id值
+	    pstmt->setString(1, usr.name);
+	    pstmt->setString(2, usr.pwd);
+	    pstmt->setString(3, usr.email);
+	    pstmt->setString(4, SQL_INIT_ADDR);
+	    pstmt->executeUpdate();
+
+		return true;
+	};
+	vector<string> Search(search_info info)	// 乘客->根据date start end查询拼车信息
+	{
+		vector<string> list;
+		pstmt = conn->prepareStatement("SELECT name, email, date, start, end, price, seat, comment FROM userinfo where date=(?) and start=(?) and end=(?)");
+		pstmt->setString(1, info.date);
+		pstmt->setString(2, info.start);
+		pstmt->setString(3, info.end);
+		res = pstmt->executeQuery();
+
+		while(res->next())					// 如果存在此拼车信息,则返回true
+		{
+			if(NULL != res)
+			{
+				list.push_back(res->getString("name"));
+				list.push_back(res->getString("email"));
+				list.push_back(res->getString("date"));
+				list.push_back(res->getString("start"));
+				list.push_back(res->getString("end"));
+				list.push_back(res->getString("price"));
+				list.push_back(res->getString("seat"));
+				list.push_back(res->getString("comment"));
+			}
+		}
+		return list;
+	};
+	bool Upload(carpool_info info)	// 车主->提交自己拼车信息
+	{
+		pstmt = conn->prepareStatement("UPDATE userinfo set date=(?), start=(?), end=(?), price=(?), seat=(?), comment=(?) where name=(?)");
+		pstmt->setString(1, info.date);
+		pstmt->setString(2, info.start);
+		pstmt->setString(3, info.end);
+		pstmt->setString(4, info.price);
+		pstmt->setString(5, info.seat);
+		pstmt->setString(6, info.comment);
+		pstmt->setString(7, info.name);
+		res = pstmt->executeQuery();
+		return true;
+	};
+	string FindAddrFromName(string name)	// 通过name判断聊天信息发往何处(fd)
+	{
+		string addr;
+		pstmt = conn->prepareStatement("SELECT * FROM userinfo where name=(?)");
+		pstmt->setString(1, name);
+		res = pstmt->executeQuery();
+    	while(res->next())		// 获取结果必须使用while()
+			addr = res->getString("addr");
+		return addr;
+	};
+	string FindNameFromAddr(int sockfd)		// 通过fd判断当前发言的人是谁
+	{
+		string addr;
+		string name;
+		stringstream tmp; 
+		tmp<<sockfd; 
+		tmp>>addr;		// int trans to string
+		pstmt = conn->prepareStatement("SELECT * FROM userinfo where addr=(?)");
+		pstmt->setString(1, addr);
+		res = pstmt->executeQuery();
+		
+    	while(res->next())		// 获取结果必须使用while()
+			name = res->getString("name");
+		return name;
+	};
+};
+
+WeSQL wesql;
+
+#endif
