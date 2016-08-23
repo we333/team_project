f236234f03c3fd8c11fe48c55af29acbb7e7fd62
qianlituo <qianlituo@gmail.com>
2016-08-23 17:09:26 +0900

    new UI

---
+++ Android_UI/app/src/main/java/com/example/dongzhe/mycarpool/splashScreen.java
@@ -0,0 +1,51 @@
+package com.example.dongzhe.mycarpool;
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+
+public class splashScreen extends AppCompatActivity {
+
+    private ImageView welcomeImg = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        welcomeImg = (ImageView) this.findViewById(R.id.welcome_img);
+        AlphaAnimation anima = new AlphaAnimation(0.3f, 1.0f);
+        anima.setDuration(2000);// 设置动画显示时间
+        welcomeImg.startAnimation(anima);
+        anima.setAnimationListener(new AnimationImpl());
+    }
+
+
+    private class AnimationImpl implements Animation.AnimationListener {
+
+        @Override
+        public void onAnimationStart(Animation animation) {
+            welcomeImg.setBackgroundResource(R.drawable.welcome);
+        }
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+            skip(); // 动画结束后跳转到别的页面
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
+
+    }
+
+    private void skip() {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+}
