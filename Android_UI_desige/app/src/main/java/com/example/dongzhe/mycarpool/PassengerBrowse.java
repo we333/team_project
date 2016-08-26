b77581e1da20c57bfc22a1ba16dea3bc70531f3f
peter <hansinrai@gmail.com>
2016-08-26 12:02:36 +0900

    success

--- Android_UI_desige/app/src/main/java/com/example/dongzhe/mycarpool/PassengerBrowse.java
+++ Android_UI_desige/app/src/main/java/com/example/dongzhe/mycarpool/PassengerBrowse.java
@@ -1,21 +1,32 @@
 package com.example.dongzhe.mycarpool;
 
 import android.content.Context;
-import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.text.AndroidCharacter;
-import android.util.AndroidException;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class PassengerBrowse extends Fragment {
 
-   Button btn_s,btn1,btn2;
+    Button btn_s,btn1;
+    EditText et_time,et_from,et_to;
+
+    String time=null;
+    String from=null;
+    String to=null;
+    String mm;
+
+    String [] strarray;
+    Client csearch = new Client();
+
+    TextView textView1,textView2,textView3,textView4,textView5,textView6,mail;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,13 +38,38 @@ public class PassengerBrowse extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_passenger_browse, container, false);
 
-        // btn_s=(Button) view.findViewById(R.id.button_p_search);
+        btn_s=(Button) view.findViewById(R.id.button_p_search);
         btn1=(Button) view.findViewById(R.id.button_pass_car);
-        //btn2=(Button) view.findViewById(R.id.button_pass_car2);
+
+        et_time=(EditText)view.findViewById(R.id.editText_search_date) ;
+        et_from=(EditText)view.findViewById(R.id.editText_search_from) ;
+        et_to=(EditText)view.findViewById(R.id.editText_search_to) ;
+
+
+
+
+        textView1=(TextView)view.findViewById(R.id.textView_s_date);
+        textView2=(TextView)view.findViewById(R.id.textView_p_from);
+        textView3=(TextView)view.findViewById(R.id.textView_p_to);
+        textView4=(TextView)view.findViewById(R.id.textView_p_price);
+        textView5=(TextView)view.findViewById(R.id.textView_p_seat);
+        textView6=(TextView)view.findViewById(R.id.textView_p_comment);
+
 
         btn1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 init();
+            }
+        });
+
+        btn_s.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                time = et_time.getText().toString();
+                from = et_from.getText().toString();
+                to = et_to.getText().toString();
+
+                new Spilt().execute();
+
             }
         });
 
@@ -44,10 +80,68 @@ public class PassengerBrowse extends Fragment {
         LayoutInflater inflater=(LayoutInflater)getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         final View layout=inflater.inflate(R.layout.passenger_browse_dialog,(ViewGroup)getActivity().findViewById(R.id.LinearLayout1));
         AlertDialog.Builder builder=new AlertDialog.Builder(getActivity());
+        TextView t = (TextView)layout.findViewById(R.id.TextView_dia_mail);
+        t.setText(mm);
         builder.setTitle("車主情報");
         builder.setView(layout);
         AlertDialog dialog=builder.create();
         dialog.show();
 
+    }
+
+    private class Spilt extends AsyncTask<String,Void,Integer> {
+        Integer sh = 0;
+        String sr;
+
+        @Override
+        protected Integer doInBackground(String... params) {
+            try {
+                if (time==null || from==null || to==null) {
+                    sh = 0;
+                } else {
+                    sr = csearch.start("search|" + time + "|" + from + "|" + to);
+
+                    sh = 1;
+                    time = null;
+                    from = null;
+                    to = null;
+                }
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return sh;
+        }
+
+        @Override
+        protected void onPostExecute(Integer integer) {
+            super.onPostExecute(integer);
+
+            if(sh==1){
+                if(sr==null){
+                    Toast.makeText(getActivity(),"no result", Toast.LENGTH_SHORT).show();
+                }else{
+                    try{
+                        System.out.println(sr);
+                        strarray=sr.split("\\|");
+
+                        btn1.setText(strarray[0]);
+                        mm = strarray[1];
+                        textView1.setText(strarray[2]);
+                        textView2.setText(strarray[3]);
+                        textView3.setText(strarray[4]);
+                        textView4.setText(strarray[5]);
+                        textView5.setText(strarray[6]);
+                        textView6.setText(strarray[7]);
+
+                    }catch(Exception e){e.printStackTrace();}
+                }
+            }
+            else {
+                Toast.makeText(getActivity(),"Please write informations", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
